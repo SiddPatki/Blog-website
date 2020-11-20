9126e07e2f015d4c66072afef996183d4d198a1b
SIDDHARTHA PATKI <siddharthapatki@gmail.com>
2020-11-20 20:41:57 +0530

    template to add new blogs & parameter based routes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+// Load the full build.
+var _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -20,8 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { Content1: homeStartingContent,composedPosts: posts });
-  
+  res.render("home", { Content1: homeStartingContent, composedPosts: posts });
 });
 
 app.get("/about", function (req, res) {
@@ -42,7 +43,22 @@ app.post("/compose", function (req, res) {
     message: req.body.composeMessage,
   };
   posts.push(composeData);
-  res.redirect('/');
+  res.redirect("/");
+});
+
+//Route parameters(keywords followed by :)
+app.get("/posts/:key", function (req, res) {
+  let match = false;
+  posts.forEach((post) => {
+    if (_.lowerCase(post.title) == _.lowerCase(req.params.key)) {
+      console.log("Match FOUND ✔");
+      match = true;
+      res.render("post", { title: post.title, message: post.message });
+    }
+  });
+  if (match == false) {
+    console.log("Match NOT found ❌");
+  }
 });
 
 app.listen(3000, function () {
